f452fc21adc137f19b1534718f686d6db543c4b2
elenagradovich <volozhinskaya@gmail.com>
2017-08-13 23:36:43 +0300

    Работа с циклами For и canvas

---
+++ js/stat.js
@@ -0,0 +1,55 @@
+'use strict';
+
+window.renderStatistics = function renderStatistics(ctx, names, times) {
+  var initialPositionTextX = 100;
+  var initialPositionTextY = 10;
+  var biasText = 10;
+  var widthMessage = 420;
+  var heightMessage = 270;
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
+  var indent = 50;
+  var initialColumnX = 150;
+  var initialColumnY = 250;
+  var width = 40;
+  var biasResultY = 5;
+  var biasNameY = 20;
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(initialPositionTextX + biasText, initialPositionTextY + biasText, widthMessage, heightMessage);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(initialPositionTextX, initialPositionTextY, widthMessage, heightMessage);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура, вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var max = -1;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+
+  for (i = 0; i < times.length; ++i) {
+
+    ctx.fillStyle = 'black';
+    ctx.fillText(Math.round(times[i]), initialColumnX + (indent + width) * i, initialColumnY - biasResultY - times[i] * step);
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'red';
+    } else {
+      var maxIndexColor = 255;
+      var saturation = Math.floor(Math.random() * maxIndexColor);
+      ctx.fillStyle = 'rgb(0, 0,' + saturation + ')';
+    }
+
+    ctx.fillRect(initialColumnX + (indent + width) * i, initialColumnY - times[i] * step, width, times[i] * step);
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], initialColumnX + (indent + width) * i, initialColumnY + biasNameY);
+  }
+
+};
